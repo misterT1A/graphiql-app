c2cd97cc63257233574e38f921f0d6481ae53c8d
Pavel <you@example.com>
2024-08-26 00:58:41 +0300

    doc: delete unused files

--- src/utils/number-to-matrix.ts
+++
@@ -1,8 +0,0 @@
-export function numberToMatrix(n: number): string[][] {
-  const outputArray: string[][] = [];
-  for (let counter = 0; counter < n; counter++) {
-    outputArray.push(new Array(0));
-  }
-  return outputArray;
-}
-
