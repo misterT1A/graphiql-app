eea56cf90289c1504e880f3824f1c439b3884a63
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-08-25 14:16:55 +0600

    fix: warning 'Prop data-selected did not match'

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,18 +2,25 @@
 
 import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import type { ReactNode } from 'react';
+import { useEffect, useState, type ReactNode } from 'react';
 
-import { useRouterIntl } from '../navigation';
+import { useRouterIntl } from '@/navigation';
 
 const Providers = ({ children }: { children: React.ReactNode }): ReactNode => {
   const router = useRouterIntl();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
 
   return (
     <NextUIProvider navigate={router.push}>
-      <NextThemesProvider attribute="class" defaultTheme="light">
-        {children}
-      </NextThemesProvider>
+      {mounted ? (
+        <NextThemesProvider attribute="class" defaultTheme="light">
+          {children}
+        </NextThemesProvider>
+      ) : (
+        children
+      )}
     </NextUIProvider>
   );
 };
