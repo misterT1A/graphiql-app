4ce2eb3ee8340feb6568eccf81cfe6a8bfe95d84
Pavel <you@example.com>
2024-08-28 19:13:17 +0300

    feat: implement internationalization for rest form

--- src/validation/schema.ts
+++ src/validation/schema.ts
@@ -1,21 +1,33 @@
+import { useTranslations } from 'next-intl';
+import type { ZodSchema } from 'zod';
 import { z } from 'zod';
 
 import { codeMirrorParser } from '@/utils/codeMirrorParser';
 
-const errorCatcher = (value: string): string => {
-  try {
-    return JSON.parse(value);
-  } catch (error) {
-    return (error as Error).message;
-  }
+const Schema = (): ZodSchema => {
+  const errorCatcher = (value: string): string => {
+    try {
+      return JSON.parse(value);
+    } catch (error) {
+      return (error as Error).message;
+    }
+  };
+
+  const t = useTranslations('RestForm');
+
+  const schema = z.object({
+    method: z.string().min(1, t('errors.method')),
+    endpoint: z.string().min(1, t('errors.endpointMin')).url(t('errors.endpointURL')),
+    headers: z.array(
+      z.object({ key: z.string().min(1, t('errors.required')), value: z.string().min(1, t('errors.required')) }),
+    ),
+    body: z.string().refine(
+      (value) => codeMirrorParser(value),
+      (value) => ({ message: `"${errorCatcher(value)}"` }),
+    ),
+  });
+
+  return schema;
 };
 
-export const schema = z.object({
-  method: z.string().min(1, 'Select method'),
-  endpoint: z.string().min(1, 'Enter endpoint URL').url('Write valid URL'),
-  headers: z.array(z.object({ key: z.string().min(1, 'Fill the field'), value: z.string().min(1, 'Fill the field') })),
-  body: z.string().refine(
-    (value) => codeMirrorParser(value),
-    (value) => ({ message: errorCatcher(value) }),
-  ),
-});
+export default Schema;
