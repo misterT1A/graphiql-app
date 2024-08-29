7e3c98360def1a14ca8d79748a6054fd96e1ec50
Pavel <you@example.com>
2024-08-30 00:04:08 +0300

    fix: change name of formRest using pascal case

--- src/app/[locale]/rest/page.tsx
+++ src/app/[locale]/rest/page.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 
-import FormRest from '@/components/formRest/formRest';
+import FormRest from '@/components/FormRest/formRest';
 
 export default function Page(): ReactNode {
   return (
