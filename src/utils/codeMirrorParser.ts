4583238753c969e077a06e02482fcfaf82459ed8
misterT1A <paradogss@mail.ru>
2024-09-06 20:44:04 +0300

    fix: fixed rest full page, formRest

--- src/utils/codeMirrorParser.ts
+++ src/utils/codeMirrorParser.ts
@@ -1,9 +1,7 @@
 export function codeMirrorParser(input: string): object | null {
   try {
-    const validJsonString = input
-      .replace(/\\n/g, '')
-      .replace(/{{\s*([a-zA-Z0-9_]+)\s*}}/g, '"{{$1}}"')
-      .replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2":');
+    const validJsonString = input.replace(/\\n/g, '').replace(/{{\s*([a-zA-Z0-9_]+)\s*}}/g, '"{{$1}}"');
+    // .replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2":');
 
     return JSON.parse(validJsonString);
   } catch {
