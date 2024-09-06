7feed4ee7a0e8fa4fe9add5d4ed5244fc5ef6226
Pavel <you@example.com>
2024-09-06 18:35:20 +0300

    feat: implement base graphForm routing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -26,6 +26,7 @@ const Header = (): ReactElement => {
   const menuItems = [
     { name: t('menuItems.home'), href: '/' },
     { name: 'RestFull client', href: '/restfull-client' },
+    { name: 'GraphQL client', href: '/graph-client' },
     // { name: 'GraphiQL client', href: '/graphiql-client' },
   ];
 
