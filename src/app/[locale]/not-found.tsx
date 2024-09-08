7297f404c599127065b031d3f7ff0628d5f063b3
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-09-08 14:49:45 +0600

    feat: style not found page, add test

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -4,5 +4,12 @@ import type { ReactElement } from 'react';
 export default function NotFound(): ReactElement {
   const t = useTranslations('NotFound');
 
-  return <div>{t('message')}</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col">
+        <p className="text-4xl text-center">404</p>
+        <p className="text-xl">{t('message')}</p>
+      </div>
+    </div>
+  );
 }
