42921c1ccd59095328706ee13112e7309ade13d7
Pavel <you@example.com>
2024-09-09 16:56:24 +0300

    fix: remove unnecessary errorCatcher function in graph schema

--- src/validation/GraphSchema.ts
+++ src/validation/GraphSchema.ts
@@ -5,15 +5,6 @@ import type { ZodSchema } from 'zod';
 import { z } from 'zod';
 
 const GraphSchema = (t: ReturnType<typeof useTranslations<'Form'>>, graphSchema: GraphQLSchema | object): ZodSchema => {
-  const errorCatcher = async (value: string): Promise<string | boolean> => {
-    if (JSON.stringify(graphSchema) === '{}') return false;
-
-    const output = await graphql({ schema: graphSchema as GraphQLSchema, source: value });
-
-    if (output.errors) return output.errors[0].message;
-    return false;
-  };
-
   const schema = z.object({
     endpoint: z.string().min(1, t('errors.endpointMin')).url(t('errors.endpointURL')),
     headers: z.array(
@@ -23,12 +14,14 @@ const GraphSchema = (t: ReturnType<typeof useTranslations<'Form'>>, graphSchema:
       z.object({ key: z.string().min(1, t('errors.required')), value: z.string().min(1, t('errors.required')) }),
     ),
     query: z.string().superRefine(async (value, ctx) => {
-      const output = await errorCatcher(value);
+      if (JSON.stringify(graphSchema) === '{}') return false;
 
-      if (output) {
+      const output = await graphql({ schema: graphSchema as GraphQLSchema, source: value });
+
+      if (output.errors) {
         ctx.addIssue({
           code: z.ZodIssueCode.custom,
-          message: `"${output}"`,
+          message: `"${output.errors[0].message}"`,
         });
       }
     }),
