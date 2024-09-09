3f2487acff8fe92755b30d60fbdd36bc27709dd8
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-09-09 20:40:48 +0600

    feat: implement home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,35 @@
+'use client';
+
+import { Link } from '@nextui-org/react';
+import { useTranslations } from 'next-intl';
 import type { ReactElement } from 'react';
 
+import { useAuth } from '@/context/AuthContext';
+
 const Home = (): ReactElement => {
+  const { user } = useAuth();
+  const t = useTranslations('Home');
+
   return (
-    <main>
-      <h1>Home</h1>
-    </main>
+    <div className="flex justify-center">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl">{user ? t('welcomeUsername', { username: user.displayName }) : t('welcome')}</h1>
+        <div className="flex gap-4">
+          {user ? (
+            <>
+              <Link href="/restfull-client">{t('buttons.restFullClient')}</Link>
+              <Link href="/graphiql-client">{t('buttons.graphiQlClient')}</Link>
+              <Link href="/history">{t('buttons.history')}</Link>
+            </>
+          ) : (
+            <>
+              <Link href="/sign-in">{t('buttons.signIn')}</Link>
+              <Link href="/sign-up">{t('buttons.signUp')}</Link>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
