6d571a0e015c37e67eaa203dbbdf1bde3fcdcaff
Pavel <you@example.com>
2024-09-14 17:21:58 +0300

    refactor: delete commented code lines in graphSchema

--- src/validation/GraphSchema.ts
+++ src/validation/GraphSchema.ts
@@ -1,5 +1,3 @@
-// import { graphql } from 'graphql';
-// import type { GraphQLSchema } from 'graphql';
 import type { useTranslations } from 'next-intl';
 import type { ZodSchema } from 'zod';
 import { z } from 'zod';
@@ -15,18 +13,6 @@ const GraphSchema = (t: ReturnType<typeof useTranslations<'Form'>>): ZodSchema =
       z.object({ key: z.string().min(1, t('errors.required')), value: z.string().min(1, t('errors.required')) }),
     ),
     query: z.string(),
-    // .superRefine(async (value, ctx) => {
-    //   if (JSON.stringify(graphSchema) === '{}') return false;
-
-    //   const output = await graphql({ schema: graphSchema as GraphQLSchema, source: value });
-
-    //   if (output.errors) {
-    //     ctx.addIssue({
-    //       code: z.ZodIssueCode.custom,
-    //       message: `"${output.errors[0].message}"`,
-    //     });
-    //   }
-    // }),
   });
 
   return schema;
